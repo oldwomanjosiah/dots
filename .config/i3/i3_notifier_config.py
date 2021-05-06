f11862ee3a5d64d050e6ea59e292d26f1c400b50
Josiah Hilden <jhilden13@gmail.com>
2021-05-05 23:06:06 -0500

    Add i3_notifer config

---
+++ .config/i3/i3_notifier_config.py
@@ -0,0 +1,21 @@
+import subprocess
+import time
+
+from i3notifier.config import Config
+from i3notifier.utils import RunAsyncFactory
+
+class DefaultConfig(Config):
+    pre_action_hooks = [
+        # Start a script to listen for urgent workspaces & switch to it
+        RunAsyncFactory(lambda n: subprocess.call("switch-to-urgent.py")),
+        # Wait for the script become available
+        lambda n: time.sleep(0.2),
+    ]
+
+
+config_list = [
+    DefaultConfig,
+]
+
+
+theme = "sidebar2" # sidebar or widget
