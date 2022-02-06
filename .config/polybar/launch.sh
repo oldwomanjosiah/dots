339085b88405b468c08f9084e2c6e44b45ee0f51
Josiah Hilden <jhilden13@gmail.com>
2022-02-05 20:07:07 -0600

    Update Polybar Setup

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,8 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar-mainlog
-polybar main -r 2>&1 | tee -a /tmp/polybar-main.log & disown
+echo "---" | tee -a /tmp/polybar-main.log
+MONITOR="DisplayPort-0" polybar left -r 2>&1 | tee -a /tmp/polybar-main.log & disown
+MONITOR="DisplayPort-2" polybar right -r 2>&1 | tee -a /tmp/polybar-main.log & disown
 
 echo "Bars launched..."
