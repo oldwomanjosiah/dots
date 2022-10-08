662ae3cfde9b35529c9a61844be50faa8abfc27b
Josiah Hilden <jhilden13@gmail.com>
2022-10-07 20:42:37 -0500

    playerctl_spot rewrite, breaking changes to json generation

--- .config/eww/scripts/playerctl_spot/src/main.rs
+++ .config/eww/scripts/playerctl_spot/src/main.rs
@@ -1,195 +1,448 @@
-use std::{
-    hash::{Hash, Hasher},
-    path::PathBuf,
-};
+use std::{hash::Hash, io::Write, path::PathBuf, time::Duration};
 
-use futures::{pin_mut, StreamExt};
+use futures::{pin_mut, Future, Stream, StreamExt};
 use once_cell::sync::Lazy;
 use regex::Regex;
 
-use tokio::io::AsyncBufReadExt;
+use anyhow::Context;
+use serde::ser::SerializeStruct;
+use tokio::io::BufReader;
+use tokio::{io::AsyncBufReadExt, select};
+use tracing::Span;
+use tracing_futures::Instrument;
 
-const FMT_STR: &str = r#"status:{{status}},title:{{title}},artist:{{artist}},album:{{album}},length:{{mpris:length}},url:{{mpris:artUrl}}"#;
-const RE_STR: &str = r#"([a-z]+):([^,]+),?"#;
+static FMT_STR: &'static str = r#"status:{{status}},title:{{title}},artist:{{artist}},album:{{album}},length:{{mpris:length}},url:{{mpris:artUrl}},position:{{position}}"#;
 
-const STORE_LOC: &str = "./images";
-const RE_REPLACE: &str = "open.spotify.com";
-const RE_REPLACE_WITH: &str = "i.scdn.co";
+static PARSE_RE: Lazy<Regex> = Lazy::new(|| Regex::new(r#"([a-z]+):([^,]+),?"#).unwrap());
 
-static PARSE_RE: Lazy<Regex> = Lazy::new(|| Regex::new(RE_STR).unwrap());
+static NAME: &str = env!("CARGO_BIN_NAME");
+static VERSION: &str = env!("CARGO_PKG_VERSION");
 
-#[derive(Debug, Clone, Default, serde::Serialize)]
-struct SpotifyData {
-    closed: bool,
-    mins: u32,
-    secs: u32,
-    pos_secs: u32,
-    progress: f32,
-    title: String,
-    playing: bool,
-    pos_str: Option<String>,
-    artist: Option<String>,
-    album: Option<String>,
-    art: Option<String>,
+macro_rules! hash {
+    ($($it:expr),+ $(,)?) => {{
+        let mut hasher = ::std::collections::hash_map::DefaultHasher::new();
+
+        $(::std::hash::Hash::hash(&($it), &mut hasher);)*
+
+        ::std::hash::Hasher::finish(&hasher)
+    }};
 }
 
-fn parse(input: String) -> Option<SpotifyData> {
-    let mut out = SpotifyData::default();
+#[derive(Debug)]
+pub struct Playerctl {
+    span: tracing::Span,
+}
 
-    for captures in PARSE_RE.captures_iter(&input) {
-        let key = captures.get(1).unwrap();
-        let value = captures.get(2).unwrap();
-
-        let key_str = key.as_str();
-        let val_str = value.as_str();
-
-        match key_str {
-            "url" => out.art = Some(val_str.into()),
-            "title" => out.title = val_str.into(),
-            "artist" => out.artist = Some(val_str.into()),
-            "album" => out.album = Some(val_str.into()),
-            "length" => {
-                let micros = val_str.parse::<usize>().unwrap();
-                let secs = micros / 1_000_000;
-
-                out.mins = (secs / 60) as u32;
-                out.secs = (secs % 60) as u32;
-            }
-            "status" => out.playing = val_str.eq_ignore_ascii_case("playing"),
-            _ => (),
+impl Playerctl {
+    pub fn new() -> Self {
+        Self {
+            span: tracing::info_span!("playerctl"),
         }
     }
 
-    if out.mins > 0 || out.secs > 0 || !out.title.is_empty() {
-        Some(out)
-    } else {
-        None
-    }
-}
+    pub fn start_watching(&self) -> anyhow::Result<impl Stream<Item = String>> {
+        let stream_span = self.span.in_scope(|| {
+            tracing::info!("Building Metadata Child");
+            tracing::info_span!("watch_child")
+        });
 
-async fn replace_image(mut data: SpotifyData) -> SpotifyData {
-    let replace = if let Some(url) = data.art {
-        let url = Regex::new(RE_REPLACE)
-            .unwrap()
-            .replace(&url, RE_REPLACE_WITH)
-            .to_string();
-
-        let mut hash = std::collections::hash_map::DefaultHasher::new();
-        url.hash(&mut hash);
-        let hash = hash.finish();
-
-        // replace open.spotify.com with i.scdn.co in the download url
-
-        let base_path = PathBuf::from(STORE_LOC);
-        std::fs::create_dir_all(&base_path).unwrap();
-        let base_path = base_path.canonicalize().unwrap();
-
-        let path = base_path.join(format!("{hash:X}"));
-
-        if !path.exists() {
-            let resp = reqwest::get(&url).await.unwrap();
-            std::fs::write(&path, resp.bytes().await.unwrap()).unwrap();
-        }
-
-        Some(path.to_string_lossy().to_string())
-    } else {
-        None
-    };
-
-    data.art = replace;
-    data
-}
-
-fn get_metadata() -> impl futures::Stream<Item = SpotifyData> {
-    async_stream::stream! {
         let mut child = tokio::process::Command::new("playerctl")
             .args(["metadata", "--follow", "--player=spotify", "-f", FMT_STR])
             .stdout(std::process::Stdio::piped())
+            .stderr(std::process::Stdio::piped())
+            .stdin(std::process::Stdio::null())
             .spawn()
-            .expect("Could not spawn playerctl");
+            .context("Spawning playerctl")?;
 
-        let reader = tokio::io::BufReader::new(child.stdout.take().unwrap());
+        let stdout_reader = BufReader::new(child.stdout.take().unwrap());
+        let stderr_reader = BufReader::new(child.stderr.take().unwrap());
 
-        let mut lines = reader.lines();
+        let inner = async_stream::stream! {
+            tracing::info!("Starting");
+            let mut stdout_lines = stdout_reader.lines();
+            let mut stderr_lines = stderr_reader.lines();
 
-        while let Some(line) = lines.next_line().await.unwrap() {
-            if let Some(line) = parse(line) {
-                yield line;
-            } else {
-                break;
+            loop {
+                select! {
+                    Ok(Some(line)) = stdout_lines.next_line() => {
+                        tracing::trace!("Got new line: {line}");
+                        yield line;
+                    },
+                    Ok(Some(line)) = stderr_lines.next_line() => {
+                        tracing::warn!("Got stderr line:\n{line}");
+                    },
+                    exit = child.wait() => {
+                        tracing::error!("Child Exited with {exit:#?}");
+                        break;
+                    },
+                }
             }
-        }
+
+            tracing::warn!("Exiting!");
+        };
+
+        Ok(tracing_futures::Instrument::instrument(inner, stream_span))
     }
 }
 
-async fn get_playtime() -> u32 {
-    let output = tokio::process::Command::new("playerctl")
-        .args(["position", "--player=spotify"])
-        .output()
-        .await
-        .unwrap()
-        .stdout;
+#[derive(Debug, Clone, Copy, Hash)]
+struct InputData<'a> {
+    pub playing: bool,
+    pub title: &'a str,
+    pub artist: &'a str,
+    pub album: &'a str,
+    pub length: &'a str,
+    pub position: &'a str,
+    pub art_url: &'a str,
+}
 
-    let output = std::str::from_utf8(output.as_ref()).unwrap();
+#[derive(Debug, Clone, Default, serde::Serialize)]
+struct SpotifyOutput {
+    pub title: String,
+    pub artist: String,
+    pub album: String,
+    pub length: TimeStamp,
+    pub position: TimeStamp,
+    pub progress: f64,
+    pub art_path: Option<PathBuf>,
+}
 
-    let (time, _) = output.split_once('.').unwrap();
+impl SpotifyOutput {
+    pub fn same_song(&self, input: &InputData<'_>) -> bool {
+        self.title == input.title && self.artist == input.artist && self.album == input.album
+    }
 
-    time.parse().unwrap()
+    pub fn update(&mut self, input: &InputData<'_>) -> bool {
+        let changed = !self.same_song(input);
+
+        if self.title != input.title {
+            self.title = input.title.into();
+        }
+
+        if self.artist != input.artist {
+            self.artist = input.artist.into();
+        }
+
+        if self.album != input.album {
+            self.album = input.album.into();
+        }
+
+        match TimeStamp::from_millis(input.length) {
+            Ok(ts) => self.length = ts,
+            Err(e) => tracing::warn!("Not updating length field: {e}"),
+        }
+
+        match TimeStamp::from_millis(input.position) {
+            Ok(ts) => self.position = ts,
+            Err(e) => tracing::warn!("Not updating position field: {e}"),
+        }
+
+        self.progress = self.position / self.length;
+
+        if changed {
+            self.art_path = None;
+        }
+
+        changed
+    }
+}
+
+#[derive(Debug, Clone, Default, serde::Serialize)]
+struct OutputData {
+    pub playing: bool,
+    pub open: bool,
+    pub song: Option<SpotifyOutput>,
+}
+
+impl OutputData {
+    pub fn update(&mut self, input: &InputData<'_>) -> bool {
+        let song_changed = match self.song.as_mut() {
+            Some(data) => data.update(input),
+            None => {
+                let mut new = SpotifyOutput::default();
+                new.update(input);
+                self.song = Some(new);
+                true
+            }
+        };
+
+        self.playing = input.playing;
+        self.open = self.open || self.playing;
+
+        song_changed
+    }
+
+    pub fn update_url(&mut self, path: PathBuf) {
+        if let Some(art_path) = self.song.as_mut().map(|it| &mut it.art_path) {
+            *art_path = Some(path);
+        }
+    }
+
+    pub fn emit(&self) -> anyhow::Result<()> {
+        let mut out = std::io::stdout().lock();
+
+        serde_json::to_writer(&mut out, self).context("Serializing data to stdout")?;
+        write!(&mut out, "\n")?;
+
+        Ok(())
+    }
+}
+
+impl<'a> InputData<'a> {
+    const NONE: &'static str = "";
+
+    fn empty() -> Self {
+        Self {
+            playing: false,
+            title: Self::NONE,
+            artist: Self::NONE,
+            album: Self::NONE,
+            length: Self::NONE,
+            position: Self::NONE,
+            art_url: Self::NONE,
+        }
+    }
+
+    pub fn from_input(input: &'a str) -> Self {
+        let mut out = Self::empty();
+
+        for captures in PARSE_RE.captures_iter(&input) {
+            let key = captures.get(1).unwrap();
+            let value = captures.get(2).unwrap();
+
+            let key_str = key.as_str();
+            let val_str = value.as_str();
+
+            match key_str {
+                "url" => out.art_url = val_str,
+                "title" => out.title = val_str,
+                "artist" => out.artist = val_str,
+                "album" => out.album = val_str,
+                "length" => out.length = val_str,
+                "position" => out.position = val_str,
+                "status" => out.playing = val_str.eq_ignore_ascii_case("playing"),
+                _ => (),
+            }
+        }
+
+        out
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct TimeStamp {
+    pub min: u16,
+    pub sec: u16,
+    pub millis: u16,
+}
+
+impl TimeStamp {
+    const MIC_CONV: usize = 1_000_000;
+    const MIL_CONV: usize = 1_000;
+    const SEC_CONV: usize = 60;
+
+    pub fn from_millis(millis: &str) -> anyhow::Result<Self> {
+        let micros: usize = millis.parse().context("Parsing as timestamp")?;
+
+        tracing::debug!("Got millis {millis}");
+
+        let it = Self {
+            min: ((micros / Self::MIC_CONV) / Self::SEC_CONV) as _,
+            sec: ((micros / Self::MIC_CONV) % Self::SEC_CONV) as _,
+            millis: (micros / Self::MIL_CONV % Self::MIL_CONV) as _,
+        };
+
+        tracing::debug!("Parsed as {it:?}");
+
+        Ok(it)
+    }
+}
+
+impl serde::Serialize for TimeStamp {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        let mut out = serializer.serialize_struct("TimeStamp", 4)?;
+        out.serialize_field("min", &self.min)?;
+        out.serialize_field("sec", &self.sec)?;
+        out.serialize_field("millis", &self.millis)?;
+        out.serialize_field("formatted", &format!("{:02}:{:02}", self.min, self.sec))?;
+        out.end()
+    }
+}
+
+impl std::ops::Div for TimeStamp {
+    type Output = f64;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        let divident =
+            (self.min as f64 * 60.0 * 1000.0) + (self.sec as f64 * 1000.0) + (self.millis as f64);
+
+        let divisor =
+            (rhs.min as f64 * 60.0 * 1000.0) + (rhs.sec as f64 * 1000.0) + (rhs.millis as f64);
+
+        divident / divisor
+    }
+}
+
+#[derive(Debug)]
+pub struct CoverArtCache {
+    span: Span,
+    cache_dir: PathBuf,
+    replacement: Regex,
+}
+
+impl CoverArtCache {
+    const CDN_URL: &'static str = "i.scdn.co";
+
+    pub fn new(cache_dir: impl Into<PathBuf>) -> anyhow::Result<Self> {
+        let cache_dir = cache_dir
+            .into()
+            .canonicalize()
+            .context("Canonicalizing cache dir")?;
+        std::fs::create_dir_all(&cache_dir).context("Creating Cache Dir")?;
+        Ok(Self {
+            span: tracing::info_span!("cover_art_cache", loc = %cache_dir.display()),
+            cache_dir,
+            replacement: Regex::new(r#"open\.spotify\.com"#).unwrap(),
+        })
+    }
+
+    pub fn get(&self, input: &str) -> impl Future<Output = anyhow::Result<PathBuf>> {
+        let working_span = self
+            .span
+            .in_scope(|| tracing::info_span!("Getting File", url = %input));
+
+        let hash_key = format!("{}", hash!(input));
+        let cached_name = {
+            let mut it = self.cache_dir.clone();
+            it.push(&hash_key);
+            it
+        };
+        let real_url = self.replacement.replace(input, Self::CDN_URL).to_string();
+
+        async move {
+            tracing::trace!("Hash Key: {hash_key}");
+
+            if cached_name.exists() {
+                tracing::info!("File already cached, returning it");
+                return Ok(cached_name);
+            }
+
+            tracing::info!("Downloading new file");
+
+            let download_res = reqwest::get(&real_url).await.context("Downloading Image")?;
+
+            tracing::info!("Download Finished");
+
+            std::fs::write(
+                &cached_name,
+                download_res
+                    .bytes()
+                    .await
+                    .context("Getting Download Contents")?,
+            )
+            .context("Writing downlaod to disk")?;
+
+            tracing::info!("Write to disk complete");
+
+            Ok(cached_name)
+        }
+        .instrument(working_span)
+    }
+}
+
+#[derive(Debug)]
+pub struct Watcher {
+    timeout: Duration,
+    playerctl: Playerctl,
+    cache: CoverArtCache,
+}
+
+async fn optional<F: Future + std::marker::Unpin>(it: &mut Option<F>) -> Option<F::Output> {
+    match it.as_mut() {
+        Some(it) => Some(it.await),
+        None => None,
+    }
+}
+
+impl Watcher {
+    pub fn new(timeout: Duration, cache_dir: impl Into<PathBuf>) -> anyhow::Result<Self> {
+        let playerctl = Playerctl::new();
+        let cache = CoverArtCache::new(cache_dir.into()).context("Creating Cache")?;
+
+        Ok(Self {
+            timeout,
+            playerctl,
+            cache,
+        })
+    }
+
+    pub fn run(self) -> impl Future<Output = anyhow::Result<()>> {
+        let task_span = tracing::info_span!("watcher");
+
+        async move {
+            tracing::info!("Starting");
+            let mut output = OutputData::default();
+            let mut download_fut = None;
+
+            let data_stream = self
+                .playerctl
+                .start_watching()
+                .context("Starting watcher stream")?;
+
+            pin_mut!(data_stream);
+
+            loop {
+                output.emit().context("Writing Output")?;
+
+                select! {
+                    Some(line) = data_stream.next() => {
+                        let data = InputData::from_input(&line);
+
+                        if output.update(&data) {
+                            // Needs update, start a download
+                            download_fut = Some(Box::pin(self.cache.get(data.art_url)));
+                        }
+                    }
+                    Some(url) = optional(&mut download_fut) => {
+                        match url {
+                            Ok(url) => output.update_url(url),
+                            Err(e) => tracing::warn!("Could not download image: {}", e),
+                        }
+                        download_fut = None;
+                    }
+                    _ = tokio::time::sleep(self.timeout), if !output.playing && output.open => {
+                        tracing::info!("Closing bar due to timeout");
+                        output.open = false;
+                    }
+                    else => {
+                        break;
+                    }
+                }
+            }
+
+            tracing::info!("Exiting");
+            Ok(())
+        }
+        .instrument(task_span)
+    }
 }
 
 #[tokio::main]
-async fn main() {
-    let mut latest_metadata = Option::<SpotifyData>::None;
-    let mut latest_playing = false;
-    let mut showing_as_playing = false;
-    let mut last_pos_secs = 0;
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
 
-    let meta_stream = get_metadata();
+    tracing::info!("Staring {NAME} ({VERSION})");
 
-    pin_mut!(meta_stream);
+    Watcher::new(Duration::from_secs(2), "./images")
+        .context("Building Watcher")?
+        .run()
+        .await
+        .context("Running Watcher")?;
 
-    let timeout = std::time::Duration::from_secs(120);
-    let track_time = std::time::Duration::from_millis(500);
-
-    loop {
-        match latest_metadata.as_mut() {
-            Some(meta) if showing_as_playing => {
-                meta.playing = latest_playing;
-                meta.pos_secs = last_pos_secs;
-                meta.progress = last_pos_secs as f32 / (meta.mins * 60 + meta.secs) as f32;
-
-                meta.pos_str.replace(format!(
-                    "{:02}:{:02}",
-                    last_pos_secs / 60,
-                    last_pos_secs % 60
-                ));
-
-                let out = serde_json::to_string(&meta).unwrap();
-                println!("{out}");
-            }
-            _ => {
-                let out = serde_json::to_string(&serde_json::json!({})).unwrap();
-                println!("{out}");
-            }
-        }
-
-        tokio::select! {
-            new_meta = meta_stream.next() => {
-                let new_meta = replace_image(new_meta.unwrap()).await;
-
-                latest_playing = new_meta.playing;
-                showing_as_playing = showing_as_playing || latest_playing;
-
-                latest_metadata.replace(new_meta);
-            },
-            _ = tokio::time::sleep(track_time), if latest_playing => {
-                let playtime = get_playtime().await;
-
-                last_pos_secs = playtime;
-            },
-            _ = tokio::time::sleep(timeout), if showing_as_playing && !latest_playing => {
-                showing_as_playing = false;
-            }
-        }
-    }
+    Ok(())
 }
